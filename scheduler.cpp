81f4130f47ab8fa66ebb1b097e30f158a8452024
Paul Schulz <paul@mawsonlakes.org>
2025-03-16 01:24:18 +1030

    Modified log messages to reflect EV charging

--- scheduler.cpp
+++ scheduler.cpp
@@ -2,13 +2,15 @@
 //////////////////////////////////////////////////////////////////////////////
 #include "scheduler.h"
 
+#define TAG "scheduler"
+
 unsigned long lastSwitchTime = 0;
 int jobIndex = 0;
 
-void job1() { ESP_LOGD("debug", "Running Job %d", 1); }
-void job2() { ESP_LOGD("debug", "Running Job %d", 2); }
-void job3() { ESP_LOGD("debug", "Running Job %d", 3); }
-void job4() { ESP_LOGD("debug", "Running Job %d", 4); }
+void job1() { ESP_LOGD(TAG, "Enable charge point %d", 1); }
+void job2() { ESP_LOGD(TAG, "Enable charge point %d", 2); }
+void job3() { ESP_LOGD(TAG, "Enable charge point %d", 3); }
+void job4() { ESP_LOGD(TAG, "Enable charge point %d", 4); }
 
 // Array of job function pointers
 void (*jobs[])() = { job1, job2, job3, job4 };
@@ -25,7 +27,7 @@ void runScheduler() {
         if (millis() - lastSwitchTime >= SWITCH_INTERVAL) {
             lastSwitchTime = millis();
             jobIndex = (jobIndex + 2) % 4;  // Rotate jobs round-robin
-            ESP_LOGD("debug", "Switching jobs...");
+            ESP_LOGD(TAG, "New charging period...");
         }
 
         sleep(1);
@@ -33,9 +35,10 @@ void runScheduler() {
 }
 
 void setup() {
-
+    ESP_LOGD(TAG,"Charging interval: %0.1fs", SWITCH_INTERVAL/1000.0);
 }
 
 void loop() {
+    ESP_LOGD(TAG,"Start scheduler");
     runScheduler();
 }
