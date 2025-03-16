4c5755121ef7614819a32d4aaf4b4d27ede02375
Paul Schulz <paul@mawsonlakes.org>
2025-03-16 11:03:05 +1030

    Make a copy of 'scheduler' as 'scheduler-simple'

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
 .PHONY: all clean
 
 all: dining \
-	scheduler
+	scheduler \
+	scheduler-simple
 
 dining: dining.c
 	gcc -o dining dining.c
 
 scheduler: main.cpp scheduler.cpp development.cpp
 	g++ -o scheduler main.cpp scheduler.cpp development.cpp
+
+scheduler-simple: main.cpp scheduler-simple.cpp development.cpp
+	g++ -o scheduler-simple main.cpp scheduler-simple.cpp development.cpp
 
 clean:
 	rm dining scheduler
---
+++ scheduler-simple.cpp
@@ -0,0 +1,58 @@
+//////////////////////////////////////////////////////////////////////////////
+#include "esphome.h"
+
+#include "scheduler.h"
+
+#define TAG "scheduler"
+
+unsigned long lastSwitchTime = 0;
+int jobIndex = 0;
+
+void job1() { ESP_LOGD(TAG, "Enable charge point %d", 1); }
+void job2() { ESP_LOGD(TAG, "Enable charge point %d", 2); }
+void job3() { ESP_LOGD(TAG, "Enable charge point %d", 3); }
+void job4() { ESP_LOGD(TAG, "Enable charge point %d", 4); }
+
+// Array of job function pointers
+void (*jobs[])() = { job1, job2, job3, job4 };
+
+void runScheduler() {
+    lastSwitchTime = millis();
+
+    while (true) {
+        // Run the two active jobs
+        jobs[jobIndex]();
+        jobs[(jobIndex + 1) % 4]();
+
+        // Check if it's time to switch
+        if (millis() - lastSwitchTime >= SWITCH_INTERVAL) {
+            lastSwitchTime = millis();
+            jobIndex = (jobIndex + 2) % 4;  // Rotate jobs round-robin
+            ESP_LOGD(TAG, "New charging period...");
+        }
+
+        sleep(1);
+    }
+}
+
+void setup() {
+    ESP_LOGD(TAG,"Charging interval: %0.1fs", SWITCH_INTERVAL/1000.0);
+}
+
+void loop() {
+    ESP_LOGD(TAG,"Start scheduler");
+    runScheduler();
+}
+
+// class MyCustomComponent : public Component {
+// public:
+//
+//     void setup() override {
+//         ESP_LOGD(TAG,"setup()");
+//     };
+//
+//     void loop() override {
+//         ESP_LOGD(TAG,"loop()");
+//     };
+//
+// } // class
